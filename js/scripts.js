a4825cd45064eaff47310063c34f165802abeed5
ezra sandzer-bell <equilibriumarts@gmail.com>
2017-08-21 13:44:30 -0700

    Commit three-step template for shoe color selection

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+  $("#second-step").hide();
+
+  $("form#nameForm").submit(function(event){
+    event.preventDefault()
+
+    var username = $("input#username").val();
+    $("#first-step").hide();
+    $("#insertname").append(username);
+    $("#second-step").show();
+  });
+
+  $("form#base-color-form").submit(function(event){
+    event.preventDefault();
+
+    var colorchoice = $("input:radio[name=base-color]:checked").val()
+    console.log(colorchoice);
+    $("#second-step").hide();
+
+    if (colorchoice == "white") {
+      $("#white-shoe").show();
+    } else if (colorchoice == "blue") {
+      $("#blue-shoe").show();
+    } else if (colorchoice == "black") {
+      $("#black-shoe").show();
+    } else if (colorchoice == "red"){
+      $("#red-shoe").show();
+    } else {
+      alert("error!");
+    }
+
+    $("#third-step").show();
+
+  })
+});
